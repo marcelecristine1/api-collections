bc293e2ed5a7e302ed257ba834a19b1d4e0b2978
marcelecristine1 <72304344+marcelecristine1@users.noreply.github.com>
2021-08-25 15:19:55 -0300

    Operações count, find e any

--- src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -22,6 +22,20 @@ fun main() {
     val salarioMaiorQue2500 = salarios.filter { it > 2500.0  }
     salarioMaiorQue2500.forEach { println(it) }
 
+    println("-------------------")
+    println("Quantos salários estão entre 2000 a 5000?")
+
+    println(salarios.count { it in 2000.0..5000.0 })
+
+    println("----------------------")
+    // buscando valores específicos
+    println(salarios.find { it == 4050.0 })
+    println(salarios.find { it == 100.0 })
+
+    println("--------------------------")
+    println(salarios.any { it == 1000.0 })
+    println(salarios.any { it == 500.0 })
+
 
 
 }
